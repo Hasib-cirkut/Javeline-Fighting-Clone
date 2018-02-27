c42a1c296faddbbb2084f1517a1989858b3b7b30
Hasib-cirkut <hasibulhuda1@gmail.com>
2018-02-27 20:58:38 +0600

    Staged

---
+++ Assets/JavelineSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavelineSpawner : MonoBehaviour {
+
+	public GameObject javeline;
+	public GameObject SpawnPoint;
+
+	
+	void Update () {
+
+		Vector3 pos = Camera.main.WorldToViewportPoint(javeline.transform.position);
+
+		if(pos.x > 1.0)
+		{
+			//Invoke("SpawnJaveline", 1.5f);
+			SpawnJaveline();
+		}		
+		
+	}
+
+	void SpawnJaveline()
+	{
+		Instantiate(javeline, SpawnPoint.transform.position, Quaternion.identity );
+	}
+}
---
+++ Assets/Scripts/Javeline.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Javeline : MonoBehaviour {
+
+	private bool isPressed = false;
+	private Rigidbody2D rb;
+
+	public SpringJoint2D joint;
+	public float releaseTime = 0.1f;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		if(isPressed)
+		{
+			rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+	}
+
+	void OnMouseDown()
+	{
+		isPressed = true;
+		rb.isKinematic = true;
+	}
+
+	void OnMouseUp()
+	{
+		isPressed = false;
+		rb.isKinematic = false;
+
+		StartCoroutine(released());
+	}
+
+	IEnumerator released()
+	{
+		yield return new WaitForSeconds(releaseTime);
+
+		joint.enabled = false;
+	}
+	
+}
